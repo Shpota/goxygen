5315cd5bacebfb489302734e48a2a6f5c53b3aca
Inna <shpota_inna@ukr.net>
2020-02-24 23:38:48 +0200

    Added Vue templates

---
+++ templates/vue.webapp/src/components/Tech.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul v-for="technology in technologies" v-bind:key="technology.name" class="technologies">
+      <li>
+        <b>{{technology.name}}</b>: {{technology.details}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Tech',
+  data() {
+    return {
+      technologies: []
+    };
+  },
+  mounted() {
+    console.log(process.env.ROOT_API)
+    axios
+      .get(`${process.env.VUE_APP_API_URL}/api/technologies`)
+      .then(response => (this.technologies = response.data));
+  }
+};
+</script>
+
+<style scoped>
+.technologies {
+  margin-top: 5px;
+}
+</style>
