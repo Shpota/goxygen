1d606c2a98032942bf54759939d08efad1eab2cc
Daniel Wolff <daniel.ryan.wolff@gmail.com>
2020-03-06 21:01:14 -0800

    fixing type errors in Tech.tsx - structuring as a typical typescript react component

--- templates/react-ts.webapp/src/tech/Tech.tsx
+++ templates/react-ts.webapp/src/tech/Tech.tsx
@@ -1,22 +1,33 @@
-import React, {Component} from "react";
+import React from "react";
 import axios from "axios";
 import "./Tech.css"
 
-export class Tech extends Component {
-    state = {
+export interface ITechnology {
+    name: string;
+    details: string;
+}
+
+export interface ITechProps {}
+
+export interface ITechState {
+    technologies: ITechnology[];
+}
+
+export class Tech extends React.Component<ITechProps, ITechState> {
+    public state = {
         technologies: []
     };
 
     componentDidMount() {
         axios.get(`${process.env.REACT_APP_API_URL}/api/technologies`)
             .then(resp => this.setState({
-                technologies: resp.data
+                technologies: resp.data as ITechnology[]
             }));
     }
 
     render() {
-        const technologies = this.state.technologies.map((technology, i) =>
-            <li key={i}>
+        const technologies = this.state.technologies.map((technology: ITechnology, index: number) =>
+            <li key={index}>
                 <b>{technology.name}</b>: {technology.details}
             </li>
         );
